e7d4c644811f09253874a476ebf702042f1e18bb
nguyenphitan <62367845+nguyenphitan@users.noreply.github.com>
2021-09-14 00:23:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,25 @@
 // Mobile
 // page 3:
 // contentPageThree
-let map = document.querySelector('.contentPageThree')
-let mapSlider = document.querySelector('.contentPageThree .threeItem1')
-
-let pressed = false;
-let startx;
-let x;
-
-map.addEventListener('mousedown', (e) => {
-    pressed = true;
-    startx = e.offsetX - mapSlider.offsetLeft;
-    map.style.cursor = 'grabbing'
-});
-
-map.addEventListener('mouseenter', () => {
-    map.style.cursor = 'grab'
-});
-
-map.addEventListener('mouseup', () => {
-    map.style.cursor = 'grab'
-});
-
-window.addEventListener('mouseup', () => {
-    pressed = false
-});
-
-map.addEventListener('mousemove', (e) => {
-    if(!pressed) return;
-    e.preventDefault();
-
-    x = e.offsetX
-    mapSlider.style.left = `${x - startx}px`;
-
-    checkboundary()
-});
-
-function checkboundary() {
-    let outer = map.getBoundingClientRect();
-    let inner = mapSlider.getBoundingClientRect();
-
-    if(parseInt(mapSlider.style.left) > 0) {
-        mapSlider.style.left = '0px';
-    }
-    else if(inner.right < outer.right) {
-        mapSlider.style.left = `-${inner.width - outer.width}px`
+let clickLeft = document.querySelector(".onclickLeft");
+let clickRight = document.querySelector(".onclickRight");
+let map = document.querySelector("#map");
+let left = 0;
+let right = 2;
+clickLeft.onclick = function() {
+    if(left == 0)   return;
+    else {
+        map.style.transform += "translateX(" + 25 + "%)"
+        left--;
+        right++;
     }
 }
 
-
+clickRight.onclick = function() {
+    if(right == 0)   return;
+    else {
+        map.style.transform += "translateX(-" + 25 + "%)"
+        right--;
+        left++;
+    }
+}
